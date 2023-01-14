572c3f9bda7793688e83d8d8a8673f82148d4af9
SrMeowers <112948674+SrMeowers@users.noreply.github.com>
2023-01-14 10:12:44 +0200

    Update joe.lua

--- joe.lua
+++ joe.lua
@@ -5,7 +5,3 @@ local old
 old = hookfunction(getupvalue(Fire, 1), function(...)
    return true
 end)
-
-local mybanks = Invoke("Get My Banks")
--- Get the player's diamond amount and remove the comma
-local mydiamonds = string.gsub(game:GetService("Players").LocalPlayer.PlayerGui.Main.Right.Diamonds.Amount.Text, "%,", "")
