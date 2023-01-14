8a1bba157f470d408dcd51bec214ae51eb6c563e
SrMeowers <112948674+SrMeowers@users.noreply.github.com>
2023-01-14 10:02:04 +0200

    Update joe.lua

--- joe.lua
+++ joe.lua
@@ -1,6 +1,7 @@
-require(game:GetService("ReplicatedStorage").Library.Client.Network)
+local Network = require(game:GetService("ReplicatedStorage").Library.Client.Network)
 local Fire, Invoke = Network.Fire, Network.Invoke
 
-local old = hookfunction(getupvalue(Fire, 1), function(...)
-  return true
+local old
+old = hookfunction(getupvalue(Fire, 1), function(...)
+   return true
 end)
