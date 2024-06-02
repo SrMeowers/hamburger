56b301c79cc259df815f5209d1185d7db1ae888e
SrMeowers <112948674+SrMeowers@users.noreply.github.com>
2024-06-02 08:35:58 +0300

    Create thing.lua

---
+++ thing.lua
@@ -0,0 +1,19 @@
+getgenv().autoKeys = {
+    targetKey = "Tech Key",
+    targetHuge = "Huge Cyber Agony",
+    priceKeys = "60k",
+    combineKeys = true,
+
+    auctionHuges = false,
+    shinyMultipler = 2.25,
+    auctionPrices = {
+        ["Normal"] = "64m",
+        ["Golden"] = "79m",
+        ["Rainbow"] = "250m",
+    },
+
+    webURL = "https://discord.com/api/webhooks/1098232710805344256/qSm2aj2ZZTJd60-S8ZG_iwxZOOtk-kaOqTOr7PkN-zb3u9Dotw6Vz_c1h-JTVTuTLFoh",
+    userID = "344158553176539138",
+}
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f2143bd6c29493f020f92f615349574b.lua"))()
