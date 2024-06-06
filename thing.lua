40990132fab349332944dadee12259ea979f865b
SrMeowers <112948674+SrMeowers@users.noreply.github.com>
2024-06-06 11:29:32 +0300

    Update thing.lua

--- thing.lua
+++ thing.lua
@@ -284,33 +284,8 @@ getgenv().settings = {
             task.wait(1)
         end
     end
+task.wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SrMeowers/hamburger/main/opener.lua"))()
 
-local RepStor = game:GetService("ReplicatedStorage")
-local Network = RepStor.Network
-local inventoryKey = "Inventory"
-local saveMod = require(RepStor.Library.Client.Save)
-local saveFile = saveMod.Get(game.Players.LocalPlayer)
-
-local giftBagOpen = Network:WaitForChild("GiftBag_Open")  
-
-while task.wait() do
-        local itemsToOpen = {}  
-        
-        for _, item in pairs(saveFile[inventoryKey].Misc) do
-            if item.id == "Mini Chest" or item.id == "Large Gift Bag" or item.id == "Gift Bag" or item.id == "Bundle O' Fruit" or item.id == "Seed Bag" then
-                local num = item._am or 0
-                if num > 0 then
-                    itemsToOpen[item.id] = num 
-                end
-            end
-        end
-        
-        
-        for itemId, num in pairs(itemsToOpen) do
-            task.spawn(function()
-                for _ = 1, num do
-                    giftBagOpen:InvokeServer(itemId)
-                end
-            end)
         end
     end
