08fcbb578f7215ca92df028972a3a31da97bd098
SrMeowers <112948674+SrMeowers@users.noreply.github.com>
2024-06-06 11:25:55 +0300

    Update thing.lua

--- thing.lua
+++ thing.lua
@@ -1,34 +1,4 @@
-local RepStor = game:GetService("ReplicatedStorage")
-local Network = RepStor.Network
-local inventoryKey = "Inventory"
-local saveMod = require(RepStor.Library.Client.Save)
-local saveFile = saveMod.Get(game.Players.LocalPlayer)
-
-local giftBagOpen = Network:WaitForChild("GiftBag_Open")  
-
-while task.wait() do
-        local itemsToOpen = {}  
-        
-        for _, item in pairs(saveFile[inventoryKey].Misc) do
-            if item.id == "Mini Chest" or item.id == "Large Gift Bag" or item.id == "Gift Bag" or item.id == "Bundle O' Fruit" or item.id == "Seed Bag" then
-                local num = item._am or 0
-                if num > 0 then
-                    itemsToOpen[item.id] = num 
-                end
-            end
-        end
-        
-        
-        for itemId, num in pairs(itemsToOpen) do
-            task.spawn(function()
-                for _ = 1, num do
-                    giftBagOpen:InvokeServer(itemId)
-                end
-            end)
-        end
-    end
-
-    getgenv().settings = {
+getgenv().settings = {
         {
             item = "Mini Chest",
             maxPrice = 33000,
@@ -314,4 +284,33 @@ while task.wait() do
             task.wait(1)
         end
     end
-    
+
+local RepStor = game:GetService("ReplicatedStorage")
+local Network = RepStor.Network
+local inventoryKey = "Inventory"
+local saveMod = require(RepStor.Library.Client.Save)
+local saveFile = saveMod.Get(game.Players.LocalPlayer)
+
+local giftBagOpen = Network:WaitForChild("GiftBag_Open")  
+
+while task.wait() do
+        local itemsToOpen = {}  
+        
+        for _, item in pairs(saveFile[inventoryKey].Misc) do
+            if item.id == "Mini Chest" or item.id == "Large Gift Bag" or item.id == "Gift Bag" or item.id == "Bundle O' Fruit" or item.id == "Seed Bag" then
+                local num = item._am or 0
+                if num > 0 then
+                    itemsToOpen[item.id] = num 
+                end
+            end
+        end
+        
+        
+        for itemId, num in pairs(itemsToOpen) do
+            task.spawn(function()
+                for _ = 1, num do
+                    giftBagOpen:InvokeServer(itemId)
+                end
+            end)
+        end
+    end
