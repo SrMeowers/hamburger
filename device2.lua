049bdf3cf80c0f5ed177251e4a49e512e43f65a8
SrMeowers <112948674+SrMeowers@users.noreply.github.com>
2024-06-23 09:03:33 +0300

    Update device2.lua

--- device2.lua
+++ device2.lua
@@ -18,3 +18,9 @@ getgenv().AutoClanWar = {
 }
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d28c9bb97737ea6426e01edc40836fea.lua"))()
+
+task.wait(3)
+getgenv().Id = "344158553176539138"
+getgenv().Url = "https://discord.com/api/webhooks/1093856436439748709/LfxwDGwXTmGStdU8UgkyJIkIcTXoiWrcmiPjcx5K0UtsoVlNdMNQV6W2WagAsyYE2zRb"
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ed88cda2afec54cf573c9c1498fad353.lua"))()
