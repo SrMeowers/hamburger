770307d6a10a25ec61824854fa110552dd3d51c6
SrMeowers <112948674+SrMeowers@users.noreply.github.com>
2024-06-23 08:42:04 +0300

    Update device2.lua

--- device2.lua
+++ device2.lua
@@ -1,1 +1,20 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4544eaf6fe42cff6d80147faef903ff7.lua"))()
+getgenv().AutoClanWar = {
+    AutoTap = true,
+    FPSBooster = true,
+    AutoFruit = true,
+    AutoUltimate = true,
+
+    AutoFlag = false,
+    FlagType = "Hasty", -- Exotic, Shiny, Rainbow, Fortune, Diamonds, Hasty, Coins, Magnet (CASE SENSETIVE)
+
+    AutoPotion = true,
+    PotionLevel = 8,
+    Potions = { -- CASE SENSITIVE
+        "Damage",
+        "Coins",
+        "Lucky",
+        "The Cocktail" -- will automatically account for no level
+    },
+}
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d28c9bb97737ea6426e01edc40836fea.lua"))()
