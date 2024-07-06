8fa9c8afd2fd9d9138737603fbce631731271922
SrMeowers <112948674+SrMeowers@users.noreply.github.com>
2024-07-06 21:27:41 +0300

    Update device2.lua

--- device2.lua
+++ device2.lua
@@ -1,25 +1,21 @@
 task.wait(10)
-
-getgenv().AutoClanWar = {
-    AutoTap = true,
-    FPSBooster = true,
-    AutoFruit = true,
-    AutoUltimate = true,
-
-    AutoFlag = false,
-    FlagType = "Hasty", -- Exotic, Shiny, Rainbow, Fortune, Diamonds, Hasty, Coins, Magnet (CASE SENSETIVE)
-
-    AutoPotion = true,
-    PotionLevel = 8,
-    Potions = { -- CASE SENSITIVE
-        "Damage",
-        "Coins",
-        "Lucky",
-        "The Cocktail" -- will automatically account for no level
-    },
-}
-
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d28c9bb97737ea6426e01edc40836fea.lua"))()
+script_key="rHTMKpaNmIPhpkXzgSqlRXhIyvESgiLx";
+-- Please Read FAQ channel for more info! Make sure you understand these parameters
+-- If not sure ask info in channel
+_G.GPROGRESS_MODE = "Hybrid"
+_G.GRANK_TO = 1
+_G.GZONE_TO = 99 -- ONLY increase above 99 to go to world2, only when 100% sure, there is no way back for the "best zone" quests etc. 
+_G.GMAX_EGG_SLOTS = 25
+_G.GMAX_EQUIP_SLOTS = 40
+_G.GHOLD_GIFTS = false
+_G.GHOLD_BUNDLES = false
+_G.GMAX_ZONE_UPGRADE_COST = 20000
+_G.GIGNORE_SLEDRACE = false
+_G.GDO_LUCKY_TILES = true
+_G.GPOTIONS = {"Damage","Coins"}
+_G.GPOTIONS_MAX_TIER = 5
+_G.GENCHANTS = {"Tap Power", "Coins", "Treasure Hunter", "Strong Pets", "Criticals"}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
 
 task.wait(3)
 getgenv().Id = "344158553176539138"
