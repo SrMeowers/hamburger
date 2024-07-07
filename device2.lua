292dee97652d172e9e7e438edb84f61632da28ad
SrMeowers <112948674+SrMeowers@users.noreply.github.com>
2024-07-07 07:03:35 +0300

    Update device2.lua

--- device2.lua
+++ device2.lua
@@ -9,6 +9,6 @@ loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d780146026110c
 
 task.wait(3)
 getgenv().Id = "344158553176539138"
-getgenv().Url = "https://discord.com/api/webhooks/1093856436439748709/LfxwDGwXTmGStdU8UgkyJIkIcTXoiWrcmiPjcx5K0UtsoVlNdMNQV6W2WagAsyYE2zRb"
+getgenv().Url = "https://discord.com/api/webhooks/1259359059551977483/P-XqQtNQ0Zh84_pLuL0Yg2y988vC_Oy6ZP7ZDxcXTPeikp-4QEvlawVSuu9blPQIqAlR"
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ed88cda2afec54cf573c9c1498fad353.lua"))()
