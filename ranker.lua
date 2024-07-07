b3cbde2fcbc9059c819301f3277f3780894825ec
SrMeowers <112948674+SrMeowers@users.noreply.github.com>
2024-07-07 16:27:03 +0300

    Update and rename mail.lua to ranker.lua

---
+++ ranker.lua
@@ -0,0 +1,17 @@
+script_key="rHTMKpaNmIPhpkXzgSqlRXhIyvESgiLx";
+-- Please Read FAQ channel for more info! Make sure you understand these parameters
+-- If not sure ask info in channel
+_G.GPROGRESS_MODE = "Hybrid"
+_G.GRANK_TO = 99
+_G.GZONE_TO = 999 -- ONLY increase above 99 to go to world2, only when 100% sure, there is no way back for the "best zone" quests etc. 
+_G.GMAX_EGG_SLOTS = 25
+_G.GDO_LUCKY_TILES = true
+_G.GMAX_EQUIP_SLOTS = 40
+_G.GHOLD_GIFTS = false
+_G.GHOLD_BUNDLES = false
+_G.GMAX_ZONE_UPGRADE_COST = 20000
+_G.GIGNORE_SLEDRACE = false
+_G.GPOTIONS = {"Damage","Coins"}
+_G.GPOTIONS_MAX_TIER = 5
+_G.GENCHANTS = {"Tap Power", "Coins", "Treasure Hunter", "Strong Pets", "Criticals"}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
