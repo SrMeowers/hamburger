964b2afe6365a9908ff244bd530ae6531c844f59
SrMeowers <112948674+SrMeowers@users.noreply.github.com>
2024-08-25 11:05:04 +0300

    Update device2.lua

--- device2.lua
+++ device2.lua
@@ -1,5 +1,19 @@
-script_key="nhQqKQFPCgWXPyePFwvWEzgciclDxYYB";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a2369ce34d44187610ac9bf5039c37ab.lua"))()
+script_key="rHTMKpaNmIPhpkXzgSqlRXhIyvESgiLx";
+-- Please Read FAQ channel for more info! Make sure you understand these parameters
+-- If not sure ask info in channel
+_G.GPROGRESS_MODE = "Hybrid"
+_G.GRANK_TO = 2
+_G.GZONE_TO = 99 -- ONLY increase above 99 to go to world2, only when 100% sure, there is no way back for the "best zone" quests etc. 
+_G.GMAX_EGG_SLOTS = 25
+_G.GMAX_EQUIP_SLOTS = 40
+_G.GHOLD_GIFTS = false
+_G.GHOLD_BUNDLES = false
+_G.GMAX_ZONE_UPGRADE_COST = 20000
+_G.GIGNORE_SLEDRACE = false
+_G.GPOTIONS = {"Damage","Coins"}
+_G.GPOTIONS_MAX_TIER = 5
+_G.GENCHANTS = {"Coins", "Treasure Hunter", "Strong Pets", "Criticals"}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
 task.wait(3)
 if not game:IsLoaded() then game.Loaded:Wait() end
 repeat wait() until game.Players
